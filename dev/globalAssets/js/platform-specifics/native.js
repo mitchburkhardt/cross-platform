71c492f25a5472690e1ebc8cb0f87f9d476d7af1
Adam Meola <adammeola@gmail.com>
2016-09-01 11:05:59 -0400

    foo

--- dev/globalAssets/js/platform-specifics/native.js
+++ dev/globalAssets/js/platform-specifics/native.js
@@ -42,6 +42,32 @@ app.checkImages = function(element, callback) {
 		}
 	}, 10);
 };
+app.nav.loadUtils = {
+	fadeIn: function(target, oldContainer, both){
+		target[0].style.zIndex = 10;
+		target[0].style.opacity = 0;
+		both.addClass('gpu');
+		setTimeout(function(){
+			target[0].style.display = 'block';
+			target[0].style.transition = 'all 300ms cubic-bezier(0.5,.5,.77,1)';
+			target[0].style.webkitTransition = 'all 300ms cubic-bezier(0.5,.5,.77,1)';
+			setTimeout(function(){
+				target[0].style.opacity = 1;
+				setTimeout(function(){
+					target.addClass('active');
+					target[0].style.zIndex = 1;
+					setTimeout(function(){
+						oldContainer[0].innerHTML = '';
+						oldContainer[0].setAttribute("style", '');
+						oldContainer[0].setAttribute("class", 'view');
+						target[0].setAttribute("style", '');
+						target[0].setAttribute("class", 'view active');
+					},10);
+				},330);
+			},10);
+		},5);
+	}
+};
 app.nav.loadParent = function(slideName, direction) {
     // TODO: load parent function for Native
 	var enterFrom = 'center';
@@ -52,29 +78,19 @@ app.nav.loadParent = function(slideName, direction) {
 	var child = $(target.find('.childslide')[localStorage.goToChild-1]);
 	target.load(slideName+'/index.html', function(){
 		app.checkImages(child, function(){
-			target[0].style.zIndex = 10;
-			target[0].style.opacity = 0;
-			both.addClass('gpu');
-			setTimeout(function(){
-				target[0].style.display = 'block';
-				target[0].style.transition = 'all 300ms cubic-bezier(0.5,.5,.77,1)';
-				setTimeout(function(){
-					target[0].style.opacity = 1;
-					setTimeout(function(){
-						target.addClass('active');
-						target[0].style.zIndex = 1;
-						setTimeout(function(){
-							oldContainer[0].innerHTML = '';
-							oldContainer[0].setAttribute("style", '');
-							oldContainer[0].setAttribute("class", 'view');
-							target[0].setAttribute("style", '');
-							target[0].setAttribute("class", 'view active');
-						},10);
-					},330);
-				},0);
-			},5);
-
-
+			switch (direction) {
+				case 'right':
+					app.nav.loadUtils.fadeIn(target, oldContainer, both);
+				break;
+				case 'left':
+					app.nav.loadUtils.fadeIn(target, oldContainer, both);
+				break;
+				case 'center':
+					app.nav.loadUtils.fadeIn(target, oldContainer, both);
+				break;
+				default:
+			}
+			app.nav.loadUtils.fadeIn(target, oldContainer, both);
 		});
 	});
 };
