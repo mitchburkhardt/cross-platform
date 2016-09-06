ed98ba8d08cc67e6827ac274750bad44602db4dd
Adam Meola <adammeola@gmail.com>
2016-09-06 11:48:39 -0400

    native parent slide transitions work!

--- dev/globalAssets/js/platform-specifics/native.js
+++ dev/globalAssets/js/platform-specifics/native.js
@@ -43,7 +43,11 @@ app.checkImages = function(element, callback) {
 	}, 10);
 };
 app.nav.loadUtils = {
+	var:{
+		inTransition: 0
+	},
 	fadeIn: function(target, oldContainer, both){
+		var that = this;
 		var duration = 300;
 		target[0].style.zIndex = 10;
 		target[0].style.opacity = 0;
@@ -63,6 +67,45 @@ app.nav.loadUtils = {
 						oldContainer[0].setAttribute("class", 'view');
 						target[0].setAttribute("style", '');
 						target[0].setAttribute("class", 'view active');
+						that.var.inTransition = 0;
+					},10);
+				},duration+30);
+			},10);
+		},5);
+	},
+	slideIn: function(target, oldContainer, both, direction){
+		var that = this;
+		var duration = 650;
+		var transition =  'all '+(duration)+'ms cubic-bezier(0,0,1,1';
+		var transition2 =  'all '+(duration*1.5)+'ms cubic-bezier(0,0,1,1)';
+		target[0].style.zIndex = 10;
+		both.addClass('gpu');
+		var numSwitch = 1;
+		if(direction === 'left') numSwitch = -1;
+		var initLeft = numSwitch*1024;
+		target[0].style.transform = 'matrix(1,0,0,1,'+initLeft+',0)';
+		target[0].style.webkitTransform = 'matrix(1,0,0,1,'+initLeft+',0)';
+		target[0].style.display = 'block';
+		setTimeout(function(){
+			target[0].style.transition = transition;
+			target[0].style.webkitTransition = transition;
+			oldContainer[0].style.transition = transition2;
+			oldContainer[0].style.webkitTransition = transition2;
+			setTimeout(function(){
+				target[0].style.transform = 'matrix(1,0,0,1,0,0)';
+				target[0].style.webkitTransform = 'matrix(1,0,0,1,0,0)';
+				oldContainer[0].style.transform = 'matrix(1,0,0,1,'+(initLeft*-1)+',0)';
+				oldContainer[0].style.webkitTransform = 'matrix(1,0,0,1,'+(initLeft*-1)+',0)';
+				setTimeout(function(){
+					target.addClass('active');
+					target[0].style.zIndex = 1;
+					setTimeout(function(){
+						oldContainer[0].innerHTML = '';
+						oldContainer[0].setAttribute("style", '');
+						oldContainer[0].setAttribute("class", 'view');
+						target[0].setAttribute("style", '');
+						target[0].setAttribute("class", 'view active');
+						that.var.inTransition = 0;
 					},10);
 				},duration+30);
 			},10);
@@ -71,6 +114,9 @@ app.nav.loadUtils = {
 };
 app.nav.loadParent = function(slideName, direction) {
     // TODO: load parent function for Native
+	var that = this;
+	if(that.loadUtils.var.inTransition) return false;
+	that.loadUtils.var.inTransition = 1;
 	var enterFrom = 'center';
 	if(direction) enterFrom = direction;
 	var both = $('#container > .view');
@@ -81,17 +127,16 @@ app.nav.loadParent = function(slideName, direction) {
 		app.checkImages(child, function(){
 			switch (direction) {
 				case 'right':
-					app.nav.loadUtils.fadeIn(target, oldContainer, both);
+					app.nav.loadUtils.slideIn(target, oldContainer, both, 'right');
 				break;
 				case 'left':
-					app.nav.loadUtils.fadeIn(target, oldContainer, both);
+					app.nav.loadUtils.slideIn(target, oldContainer, both, 'left');
 				break;
 				case 'center':
 					app.nav.loadUtils.fadeIn(target, oldContainer, both);
 				break;
 				default:
 			}
-			app.nav.loadUtils.fadeIn(target, oldContainer, both);
 		});
 	});
 };
